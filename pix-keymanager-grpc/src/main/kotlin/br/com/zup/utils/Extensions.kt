c5c3f666ee61dbbddb83e2a036f4c2d32f02d0ad
Adrici <cirellia5@gmail.com>
2021-06-24 15:32:14 -0300

    Consultando os dados de uma chave Pix

--- pix-keymanager-grpc/src/main/kotlin/br/com/zup/utils/Extensions.kt
+++ pix-keymanager-grpc/src/main/kotlin/br/com/zup/utils/Extensions.kt
@@ -1,12 +1,16 @@
 package br.com.zup.utils
 
-
 import br.com.zup.chave.NovaChavePix
+import br.com.zup.consulta.Filtro
+import br.com.zup.grpc.ConsultaChavePixRequest
+import br.com.zup.grpc.ConsultaChavePixRequest.FiltroCase.*
 import br.com.zup.grpc.RegistraChavePixRequest
 import br.com.zup.grpc.TipoDeConta.*
 import br.com.zup.grpc.TipoDeChave.*
 import br.com.zup.grpc.TipoDeConta
 import br.com.zup.registra.TipoDeChaveRegex
+import javax.validation.ConstraintViolationException
+import javax.validation.Validator
 
 fun RegistraChavePixRequest.toModel(): NovaChavePix {
 
@@ -22,4 +26,22 @@ fun RegistraChavePixRequest.toModel(): NovaChavePix {
             else -> TipoDeConta.valueOf(tipoDeConta.name)
         }
     )
+}
+
+fun ConsultaChavePixRequest.toModel(validator: Validator): Filtro {
+
+    val filtro = when(filtroCase!!) {
+        PIXID -> pixId.let {
+            Filtro.PorPixId(clientId = it.clientId, pixId = it.pixId)
+        }
+        CHAVE -> Filtro.PorChave(chave)
+        FILTRO_NOT_SET -> Filtro.Invalido()
+    }
+
+    val violations = validator.validate(filtro)
+    if (violations.isNotEmpty()) {
+        throw ConstraintViolationException(violations);
+    }
+
+    return filtro
 }
